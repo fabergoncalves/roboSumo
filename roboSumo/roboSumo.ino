fa15646b552e15534b13e00e41dfe21d8d6111d2
Fabricio Gonçalves <211.fabricio@gmail.com>
2017-11-05 11:43:15 -0200

    Engines setup with Interrupts

--- roboSumo/roboSumo.ino
+++ roboSumo/roboSumo.ino
@@ -9,7 +9,12 @@
 #define buttonStart 12
 #define frontStrip 0
 #define rearStrip 1
-
+#define IN1 4
+#define IN2 6
+#define velocidadeA 5
+#define IN3 7
+#define IN4 8
+#define velocidadeB 9
 
 //Variables
 Servo servoControl;
@@ -28,6 +33,36 @@ void rearDetected(){
 	rearStripFlag = 1;
 }
 
+void parar(){
+	analogWrite(velocidadeA,0);
+	analogWrite(velocidadeB,0);
+	digitalWrite(IN1,LOW);
+	digitalWrite(IN2,LOW);
+	digitalWrite(IN3,LOW);
+	digitalWrite(IN4,LOW);
+}
+
+void moveFrenteFaixa()
+{
+	analogWrite(velocidadeA,100);
+	digitalWrite(IN1,LOW);
+	digitalWrite(IN2,HIGH);
+	analogWrite(velocidadeB,100);
+	digitalWrite(IN3,LOW);
+	digitalWrite(IN4,HIGH);
+}
+
+void moveTrasFaixa()
+{
+	analogWrite(velocidadeA,100);
+	digitalWrite(IN1,HIGH);
+	digitalWrite(IN2,LOW);
+	analogWrite(velocidadeB,100);
+	digitalWrite(IN3,HIGH);
+	digitalWrite(IN4,LOW);
+}
+
+//Common arduino functions
 void setup() {
 	Serial.begin(9600);
 
@@ -37,6 +72,14 @@ void setup() {
 
 	//Setup start button
 	pinMode(buttonStart, INPUT);
+
+	//Setup engine
+	pinMode(IN1,OUTPUT);
+	pinMode(IN2,OUTPUT);
+	pinMode(IN3,OUTPUT);
+	pinMode(IN4,OUTPUT);
+	pinMode(velocidadeA,OUTPUT);
+	pinMode(velocidadeB,OUTPUT);
 
 	//Setup interrupt Strip sensors
 	attachInterrupt(frontStrip,frontDetected,FALLING);
@@ -57,14 +100,18 @@ void loop() {
 	
 	if(frontStripFlag == 1){
 		Serial.println("Back");
+		moveTrasFaixa();
 		delay(3000);
 		frontStripFlag = 0;
+		parar();
 	}
 
 	if(rearStripFlag == 1){
 		Serial.println("Front");
+		moveFrenteFaixa();
 		delay(3000);
-		rearStripFlag = 0;	
+		rearStripFlag = 0;
+		parar();
 	}
 
 	
